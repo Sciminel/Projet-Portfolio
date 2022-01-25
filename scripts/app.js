adfc28e5fc10b4f4dd7b6d3988d711dc1a3299d1
Sciminel <abdel.barkaoui@gmail.com>
2022-01-25 19:18:28 +0100

    Mise en place d'un slider en accueil, modif de la nav bar, mise en place de bootstrap

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,10 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menuDeroulant.classList.toggle('active');
 })
+
+
+// Réduire le temps de défilement des images pour créer une fluidité 
+// d'une arriere map (exemple: load runner)
+const vitesseCarousel = document.querySelector('.carousel-inner');
+const imgCarousel = document.querySelector('.carousel-item');
+const active = document.querySelector('.active');
