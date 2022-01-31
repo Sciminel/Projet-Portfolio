1451282da88eb9114e0515771f8f54ad851fe578
Barkaoui Abdelkrim <53445220+Sciminel@users.noreply.github.com>
2022-01-31 21:19:49 +0100

    Fin de la mise en place de la structure, quelques détails, du js et des img a regler

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,26 @@
 const burger = document.querySelector(".btn1");
 const menuDeroulant = document.querySelector(".list-nav");
+const btnGalerie = document.querySelector('.btn-project');
+const galerie = document.querySelector('.project-container');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menuDeroulant.classList.toggle('active');
 })
 
+
+btnGalerie.addEventListener('click', () => {
+    galerie.classList.toggle('affiche');
+    console.log(galerie)
+})
+
 const bienvenu = document.querySelector('.title-custom');
 
-//const popupName = prompt("Quel est votre nom ? (La donnée ne sera pas enregistré)");
+// const popupName = prompt("Quel est votre nom ? (La donnée ne sera pas enregistré)");
 
 
-if(popupName){
-    bienvenu.innerHTML = `Bienvenu(e) ${popupName}`;
-}else{
-    bienvenu.innerHTML = `Bienvenu jeune inconnu !aaa`
-}
+// if(popupName){
+//     bienvenu.innerHTML = `Bienvenu(e) ${popupName}`;
+// }else{
+//     bienvenu.innerHTML = `Bienvenu jeune inconnu !`
+// }
